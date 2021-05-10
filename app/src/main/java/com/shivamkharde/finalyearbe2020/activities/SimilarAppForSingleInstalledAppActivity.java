0e0f9d82de4a72d9b86b5d027c882cd83ef4be1a
shivamkharde <shivam121820@gmail.com>
2021-05-10 23:59:34 +0530

    change the way of calling api ...now calling from similar apps list adapter instead of calling it from next activity

--- app/src/main/java/com/shivamkharde/finalyearbe2020/activities/SimilarAppForSingleInstalledAppActivity.java
+++ app/src/main/java/com/shivamkharde/finalyearbe2020/activities/SimilarAppForSingleInstalledAppActivity.java
@@ -1,61 +1,45 @@
 package com.shivamkharde.finalyearbe2020.activities;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Parcelable;
 
 import org.json.*;
 import com.loopj.android.http.*;
 import com.shivamkharde.finalyearbe2020.R;
 import com.shivamkharde.finalyearbe2020.models.SimilarAppHTTPClient;
 
+import java.text.NumberFormat;
 import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
 
 public class SimilarAppForSingleInstalledAppActivity extends AppCompatActivity {
 
+    private ProgressDialog p;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_similar_app_for_single_installed_app);
-//        get intent values
-        Bundle extras = getIntent().getExtras();
-        final String packageName = extras.getString("packageName");
 
-//        call an api to print application related to this package name\
+
         try {
-            getAllSimilarApplications(packageName);
-        } catch (Exception e) {
+
+            Bundle b = getIntent().getExtras();
+            JSONObject p = new JSONObject(b.getString("response"));
+            System.out.println(p);
+
+        } catch (JSONException e) {
             e.printStackTrace();
         }
+
     }
 
-    private void getAllSimilarApplications(String packageName)throws Exception {
-        SimilarAppHTTPClient.get("api/apps/"+packageName+"/similar/", null, new JsonHttpResponseHandler(){
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                // If the response is JSONObject instead of expected JSONArray
-                try {
-                    JSONArray arr = response.getJSONArray("results");
-                    JSONObject obj = arr.getJSONObject(1);
-                    System.out.println(obj.getString("title"));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
-//                // Pull out the first event on the public timeline
-//                JSONObject firstEvent = timeline.get(0);
-//                String tweetText = firstEvent.getString("text");
-//
-//                // Do something with the response
-//                System.out.println(tweetText);
-            }
-        });
-    }
 }
--- app/src/main/java/com/shivamkharde/finalyearbe2020/adapters/SimilarAppInstalledListAdapter.java
+++ app/src/main/java/com/shivamkharde/finalyearbe2020/adapters/SimilarAppInstalledListAdapter.java
@@ -1,9 +1,12 @@
 package com.shivamkharde.finalyearbe2020.adapters;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Drawable;
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,14 +14,23 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.loopj.android.http.JsonHttpResponseHandler;
 import com.shivamkharde.finalyearbe2020.R;
 import com.shivamkharde.finalyearbe2020.activities.SimilarAppForSingleInstalledAppActivity;
+import com.shivamkharde.finalyearbe2020.models.SimilarAppHTTPClient;
 
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
 
 public class SimilarAppInstalledListAdapter extends RecyclerView.Adapter<SimilarAppInstalledListAdapter.SimilarAppsInstalledListViewHolder> {
 
@@ -55,14 +67,133 @@ public class SimilarAppInstalledListAdapter extends RecyclerView.Adapter<Similar
         holder.similarAppsInstalledApplicationCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                following code is for testing purpose remove after implementing above
-                Intent similarAppForSingleInstalledAppIntent = new Intent(applicationContext, SimilarAppForSingleInstalledAppActivity.class);
-//                passing single application info details
-                similarAppForSingleInstalledAppIntent.putExtra("packageName",applicationPackageName);
-                similarAppForSingleInstalledAppIntent.putExtra("applicationName",applicationName);
-                applicationContext.startActivity(similarAppForSingleInstalledAppIntent);
+//                call an api to search for the similar app for this package name if not available then search by its name
+                try{
+                    getAllSimilarApplicationsByPackageName(applicationPackageName, (String) applicationName);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
             }
         });
+    }
+
+//    this method is to get all the similar applications by its package name and if not available then by its application name
+    private void getAllSimilarApplicationsByPackageName(String packageName,String applicationName) {
+
+//        show progress dialog
+        ProgressDialog p = new ProgressDialog(applicationContext);
+        p.setMessage("Please wait while we are fetching details!!!");
+        p.setCancelable(false);
+        p.setMax(100);
+        p.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        p.show();
+
+//        request an API
+        SimilarAppHTTPClient.get("api/apps/"+packageName+"/similar/", null, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // If the response is JSONObject instead of expected JSONArray
+                p.dismiss();
+                try {
+//                    on success get all the necessary data and passit through intent to show the list of similar apps to the user on next page
+//                    create an intent to switch the window with response data
+                    Intent similarAppForSingleInstalledAppIntent = new Intent(applicationContext, SimilarAppForSingleInstalledAppActivity.class);
+//                    passing response data as a intent data to the next activity
+                    similarAppForSingleInstalledAppIntent.putExtra("response", response.toString());
+//                passing single application info details
+                    applicationContext.startActivity(similarAppForSingleInstalledAppIntent);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+                p.dismiss();
+//                create a dialog to show the error
+                AlertDialog.Builder al = new AlertDialog.Builder(applicationContext);
+                al.setMessage("Similar Data not available for this app!! Do you want to search the apps similar to this name (data might not be accurate)");
+                al.setCancelable(false);
+                al.setTitle("Not Found!!");
+                al.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+//                        call another function to search application by its name
+                        getAllSimilarApplicationsByApplicationName(applicationName);
+                    }
+                });
+                al.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+//                        do nothing and hide the progress dialog and alert dialog
+                        p.dismiss();
+                    }
+                });
+                al.show();
+            }
+
+            @Override
+            public void onProgress(long bytesWritten, long totalSize) {
+                super.onProgress(bytesWritten, totalSize);
+                int progress = (int) ((bytesWritten*100)/totalSize);
+                p.setProgress(progress);
+            }
+        });
+    }
+
+//    this method is to get all the similar apps by its application name
+    private void getAllSimilarApplicationsByApplicationName(String applicationName) {
+//        show progress dialog
+        ProgressDialog p = new ProgressDialog(applicationContext);
+        p.setMessage("Searching with application name...Please wait");
+        p.setCancelable(false);
+        p.setMax(100);
+        p.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        p.show();
+
+//        request an API
+        SimilarAppHTTPClient.get("api/apps/?q="+applicationName, null, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // If the response is JSONObject instead of expected JSONArray
+                p.dismiss();
+                try {
+//                    on success get all the necessary data and pass it through intent to show the list of similar apps to the user on next page
+//                    create an intent to switch the window with response data
+                    Intent similarAppForSingleInstalledAppIntent = new Intent(applicationContext, SimilarAppForSingleInstalledAppActivity.class);
+//                    passing response data as a intent data to the next activity
+                    similarAppForSingleInstalledAppIntent.putExtra("response", response.toString());
+//                    passing single application info details
+                    applicationContext.startActivity(similarAppForSingleInstalledAppIntent);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+                p.dismiss();
+//                create a dialog to show the error
+                AlertDialog.Builder al = new AlertDialog.Builder(applicationContext);
+                al.setMessage("Similar Data not available for this app!! Do you want to search the apps similar to this name (data might not be accurate)");
+                al.setCancelable(false);
+                al.setTitle("Not Found!!");
+                al.setPositiveButton("OK", null);
+                al.show();
+            }
+
+            @Override
+            public void onProgress(long bytesWritten, long totalSize) {
+                super.onProgress(bytesWritten, totalSize);
+                int progress = (int) ((bytesWritten*100)/totalSize);
+                p.setProgress(progress);
+            }
+        });
+
     }
 
     @Override
